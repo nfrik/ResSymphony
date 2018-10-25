87960fcb543bafff507c1d64804e7b7cbb6e5282
frix <nfrik@emetx.com>
2018-10-25 19:28:46 -0400

    modified setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='ResSymphony',
+setup(name='ressymphony',
       version='0.1.3c',
       description='Controller for CircuitSymphony - distributed SPICE',
-      url='https://github.com/nfrik/ressymphony',
+      url='https://github.com/nfrik/ResSymphony',
       author='Nikolay Frick',
       author_email='nvfrik@ncsu.edu',
       license='MIT',
