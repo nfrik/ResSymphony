bad10aa5c159f81b48d1594cca0585b6201ce688
frix <nfrik@emetx.com>
2018-10-25 19:23:38 -0400

    modified setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='ressymphony',
-      version='0.1.3b',
+setup(name='ResSymphony',
+      version='0.1.3c',
       description='Controller for CircuitSymphony - distributed SPICE',
       url='https://github.com/nfrik/ressymphony',
       author='Nikolay Frick',
