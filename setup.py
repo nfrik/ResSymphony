010c7a65c99199b09879ac65fe017584727da02e
frix <nfrik@emetx.com>
2018-10-26 10:21:38 -0400

    added evolutionary search

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ressymphony',
-      version='0.1.3c',
+      version='0.1.4c',
       description='Controller for CircuitSymphony - distributed SPICE',
       url='https://github.com/nfrik/ResSymphony',
       author='Nikolay Frick',
@@ -9,6 +9,6 @@ setup(name='ressymphony',
       license='MIT',
       packages=['resutils'],
       install_requires=[
-          'tqdm',
+          'tqdm','sklearn-deap'
       ],
       zip_safe=False)
