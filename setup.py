065a68cd9dbd959eb20c5c88c979824ca77e4dd9
frix <nfrik@emetx.com>
2018-10-26 10:12:56 -0400

    added tqdm dependency and bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='resutils',
-      version='0.1.3c',
+      version='0.1.4a',
       description='Controller for CircuitSymphony - distributed SPICE',
       url='https://github.com/nfrik/ResSymphony',
       author='Nikolay Frick',
